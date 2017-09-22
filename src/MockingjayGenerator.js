cd57b2820d3cfaf600b81529e1dcb0be31f54bc2
Micha Mazaheri <micha@mazaheri.me>
2017-09-22 10:36:17 +0200

    Fix JSON to Swift

--- src/MockingjayGenerator.js
+++ src/MockingjayGenerator.js
@@ -11,39 +11,63 @@ class MockingjayGenerator {
   _generateMatcher(request) {
     const method = request.method.toUpperCase()
     if (method === 'GET') {
-      return `uri("${request.url}")`
+      return `uri("${request.urlBase}")`
     }
     else {
-      return `http(.${method}, "${request.url}")`
+      return `http(.${method}, "${request.urlBase}")`
     }
   }
 
   _generateBuilder(httpExchange) {
     const contentType = httpExchange.responseHeaders['Content-Type']
     if (contentType.match(/^application\/json/)) {
-      return `json(${this._generateJsonObject(JSON.parse(httpExchange.responseBody))}, status: ${httpExchange.responseStatusCode})`
+      let jsonObject
+      let swiftJsonString
+      try {
+        jsonObject = JSON.parse(httpExchange.responseBody)
+      }
+      catch (e) {
+        console.error(`Invalid JSON response: ${e.toString()}`)
+        jsonObject = null
+      }
+      if (jsonObject !== null) {
+        swiftJsonString = this._generateJsonObject(jsonObject)
+      }
+      else {
+        swiftJsonString = '/* JSON response */'
+      }
+      return `json(${swiftJsonString}, status: ${httpExchange.responseStatusCode})`
     }
     return `http(${httpExchange.responseStatusCode})`
   }
 
   _generateJsonObject(jsonObject, indent = 0) {
-    if (Array.isArray(jsonObject)) {
+    if (jsonObject === null) {
+      return 'NSNull.null()'
+    }
+    else if (typeof jsonObject === 'string') {
+      return `"${jsonObject}"`
+    }
+    else if (Array.isArray(jsonObject)) {
+      if (jsonObject.length === 0) {
+        return '[]'
+      }
       const children = jsonObject.map(obj => {
         return `${'  '.repeat(indent + 1)}${this._generateJsonObject(obj, indent + 1)}`
       })
       return `[\n${children.join(',\n')}\n${'  '.repeat(indent)}]`
     }
     else if (typeof jsonObject === 'object') {
+      if (jsonObject.length === 0) {
+        return '[:]'
+      }
       const children = Object.keys(jsonObject).map(key => {
         return `${'  '.repeat(indent + 1)}"${key}": ${this._generateJsonObject(jsonObject[key], indent + 1)}`
       })
       return `[\n${children.join(',\n')}\n${'  '.repeat(indent)}]`
     }
-    else if (typeof jsonObject === 'string') {
-      return `"${jsonObject}"`
-    }
     else {
-      return jsonObject
+      return jsonObject.toString()
     }
   }
 
