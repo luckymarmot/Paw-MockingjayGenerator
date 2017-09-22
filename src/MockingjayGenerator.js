031d52db76ceb0418b7ff332fc4646a807bfcf1a
Micha Mazaheri <micha@mazaheri.me>
2017-09-22 10:25:03 +0200

    Fix JSON indent

--- src/MockingjayGenerator.js
+++ src/MockingjayGenerator.js
@@ -28,14 +28,16 @@ class MockingjayGenerator {
 
   _generateJsonObject(jsonObject, indent = 0) {
     if (Array.isArray(jsonObject)) {
-      const children = jsonObject.map(obj => this._generateJsonObject(obj, indent + 1))
-      return `[\n${'  '.repeat(indent)}${children.join(',\n')}]`
+      const children = jsonObject.map(obj => {
+        return `${'  '.repeat(indent + 1)}${this._generateJsonObject(obj, indent + 1)}`
+      })
+      return `[\n${children.join(',\n')}\n${'  '.repeat(indent)}]`
     }
     else if (typeof jsonObject === 'object') {
       const children = Object.keys(jsonObject).map(key => {
-        return `${'  '.repeat(indent)}"${key}": ${this._generateJsonObject(jsonObject[key], indent + 1)}`
+        return `${'  '.repeat(indent + 1)}"${key}": ${this._generateJsonObject(jsonObject[key], indent + 1)}`
       })
-      return `[\n${children.join(',\n')}]`
+      return `[\n${children.join(',\n')}\n${'  '.repeat(indent)}]`
     }
     else if (typeof jsonObject === 'string') {
       return `"${jsonObject}"`
